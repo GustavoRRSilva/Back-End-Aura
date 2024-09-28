9b98bfbd3d61a99318d8a9d5baa905fc169f790f
Gustavo <gstvrrds@gmail.com>
2024-09-28 10:56:56 -0300

    feat:dando bem vindo a aura

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send("Seja bem vindo a Aura!");
 });
 
 // Exporta o app como uma função
